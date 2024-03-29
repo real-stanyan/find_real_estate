d1674b14814eedcabeaf4f6cc178c4706370e701
Stan Yan <stanhavenoidea@gmail.com>
2024-03-29 20:10:11 +1100

    init backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// import pages
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 
+// import components
+import Header from "./components/Header";
+
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
