3698fffe28d0e5b22b5b8de19f8cee1f687f89ad
Stan Yan <stanhavenoidea@gmail.com>
2024-03-29 18:59:32 +1100

    init pages

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-[red]">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptate
+      soluta nemo! Ducimus adipisci error consequuntur. Sed repudiandae sequi
+      eius possimus nam, eos velit aspernatur nobis, harum corporis dicta
+      asperiores.
+    </div>
+  );
+};
+
+export default Home;
