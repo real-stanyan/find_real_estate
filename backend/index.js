24a1bc8593b0512258a39d706552b094e48f287a
Stan Yan <stanhavenoidea@gmail.com>
2024-03-29 23:19:17 +1100

    signup function done

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -17,8 +19,10 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 // middleware
 app.use((err, req, res, next) => {
